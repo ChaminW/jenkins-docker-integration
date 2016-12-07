42f2fb875cc148ae3d77e77b6af1ba2e3fa978f1
Chamin WIckramarathna <chamin.13@cse.mrt.ac.lk>
2016-12-07 14:49:05 +0530

    Update commitNotify.sh

--- commitNotify.sh
+++ commitNotify.sh
@@ -1,4 +1,6 @@
-echo **************************************************************
-echo Git commit is triggered in jenkins-docker intergration project
-echo **************************************************************
-echo **************************************************************
+echo ******************************************************
+echo Git commit is triggered in jenkins-docker intergration
+echo ******************************************************
+cd /var/www/html/docker-project/jenkins
+docker rm jenkins-test -f 
+docker run --name jenkins-test -d ubuntu:apache-mysql /bin/sh -c "while true; do echo hello world; sleep 1; done"
